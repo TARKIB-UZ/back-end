18ad7c94ee96bf71e8e95cf65a4eaba550d0fc39
nomonovn2 <nomonovn2@gmail.com>
2024-06-24 19:13:33 +0000

    template pushed

---
+++ .idx/dev.nix
@@ -0,0 +1,22 @@
+{ pkgs, ... }: {
+  # Which nixpkgs channel to use.
+  channel = "stable-23.11"; # or "unstable"
+  # Use https://search.nixos.org/packages to find packages
+  packages = [
+    pkgs.go
+    pkgs.nano
+    pkgs.docker
+    pkgs.git
+  ];
+
+  services.docker.enable=true;
+  # Sets environment variables in the workspace
+  env = {};
+  idx = {
+    # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
+    extensions = [
+      "golang.go"
+    ];
+    # Enable previews and customize configuration   
+  };
+}
